0c54c0f987ad8cd3176e543d9682dbb13e91504a
ci-sumi <tharayilsurendransumi@gmail.com>
2024-12-17 19:37:11 +0000

    Implement category filtering with dropdown menu on product page

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,get_object_or_404
-from .models import Product
+from .models import Product, Category
 from django.db.models import Q
 from django.contrib import messages
 
@@ -8,6 +8,7 @@ from django.contrib import messages
 # Create your views here.
 def all_products(request):
     query = request.GET.get('q')
+    category = request.GET.get('category')
     products = Product.objects.all()
     range_of_stars = range(1,6)
     if query:
@@ -16,10 +17,13 @@ def all_products(request):
             Q(description__icontains=query))
     if not products.exists():
         messages.error(request,'No results found')
+    if category:
+        products = Product.objects.filter(category__name=category)
     context = {
         'products':products,
         'range_of_stars':range_of_stars,
-        'query':query
+        'query':query,
+        'selected_category':category
     }
     return render(request,'products/products.html',context)
 
