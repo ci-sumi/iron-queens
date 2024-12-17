6ac7a07d5e77460777c457a79fccf7981d8a16ee
ci-sumi <tharayilsurendransumi@gmail.com>
2024-12-17 18:38:51 +0000

    Add product search functionality by name and description in all_products view

--- products/views.py
+++ products/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Product
+from django.db.models import Q
+from django.contrib import messages
+
 
 
 # Create your views here.
 def all_products(request):
+    query = request.GET.get('q')
     products = Product.objects.all()
     range_of_stars = range(1,6)
+    if query:
+        products = Product.objects.filter(
+            Q(name__icontains=query) |
+            Q(description__icontains=query))
+    if not products.exists():
+        messages.error(request,'No results found')
     context = {
         'products':products,
-        'range_of_stars':range_of_stars
+        'range_of_stars':range_of_stars,
+        'query':query
     }
     return render(request,'products/products.html',context)
 
