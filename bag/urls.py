dacc4e9ab826ec7531ad604d268bbbe1d8828bbd
ci-sumi <tharayilsurendransumi@gmail.com>
2024-12-19 21:59:02 +0000

    Add URL pattern for adding items to the shopping bag

--- bag/urls.py
+++ bag/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path
 from .import views
 
+
 urlpatterns = [
     path('',views.view_bag, name='view_bag'),
+     path('add-to-bag/<int:product_id>/', views.add_to_bag, name='add_to_bag'),
     
 ]
 
