fc4daffea5f3c682e2034de877bfe1e59f2ea09b
ci-sumi <tharayilsurendransumi@gmail.com>
2024-12-28 09:17:40 +0000

    Add checkout view with bag check and order form

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import OrderForm
+from django.contrib import messages
 
 # Create your views here.
+def checkout(request):
+    bag = request.session.get('bag', {})
+    if not bag:
+        messages.error(request, "Your bag is empty")
+        return redirect('products')
+    orderform = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'orderform':orderform
+    }
+    return render(request, template, context)
