29cf7ec35bacdf32a53be11f0378240e7c9a7bde
ci-sumi <tharayilsurendransumi@gmail.com>
2024-12-28 13:16:04 +0000

    Add CountryField to Order model and for country selection

--- checkout/models.py
+++ checkout/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.db.models import Sum
 from django.conf import settings
 from products.models import Product
+from django_countries.fields import CountryField 
 
 # Create your models here.
 class Order(models.Model):
@@ -10,12 +11,12 @@ class Order(models.Model):
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
-    country = models.CharField(max_length=40, null=False, blank=False)
+    county = models.CharField(max_length=40, null=False, blank=False)
     postcode = models.CharField(max_length=20, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
-    county = models.CharField(max_length=40, null=False, blank=False)
+    country = CountryField(blank_label='select country', null=True)
     date = models.DateTimeField(auto_now_add=True)
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=6,decimal_places=2, null=False, default=0 )
