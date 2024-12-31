c559dc2e75bda5060f22729d9f1904a1e8e3b01c
ci-sumi <tharayilsurendransumi@gmail.com>
2024-12-31 08:43:40 +0000

    Update signals to update Order totals on save and delete

--- checkout/signals.py
+++ checkout/signals.py
@@ -5,11 +5,9 @@ from .models import OrderLineItem,Order
 @receiver(post_save, sender=OrderLineItem)
 def update_on_save(sender, instance, created, **kwargs):
     if created:
-        instance.order.update_total()
-        instance.update_order_total()
+        instance.order.update_order_total() 
 
 @receiver(post_delete, sender=OrderLineItem)
 def update_on_delete(sender, instance, **kwargs):
-    instance.order.update_total()
     instance.order.update_order_total()
         
