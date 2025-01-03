ec299561997476b4b5cccdc0dfca5edf5f853154
ci-sumi <tharayilsurendransumi@gmail.com>
2025-01-03 22:47:01 +0000

    Modify create_user_profile function

--- profiles/models.py
+++ profiles/models.py
@@ -19,10 +19,13 @@ class UserProfile(models.Model):
     
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
-        if created:
-            UserProfile.objects.create(user=instance)
-        instance.userprofile.save() 
-    
+    """
+    Create or update the user profile
+    """
+    if created:
+        UserProfile.objects.create(user=instance)
+    # Existing users: just save the profile
+    instance.userprofile.save()
     
            
     
