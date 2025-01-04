9234e79f76305de0378fb4f666e07bba8ed9c609
ci-sumi <tharayilsurendransumi@gmail.com>
2025-01-04 15:12:13 +0000

    Replace CharField with CountryField for country selection

--- profiles/models.py
+++ profiles/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
+from django_countries.fields import CountryField
 # Create your models here.  
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -12,7 +12,7 @@ class UserProfile(models.Model):
     default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
     default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
     default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
-    default_country = models.CharField(max_length=40, null=True, blank=True)
+    default_country = CountryField(blank_label='Country',max_length=40,null=True, blank=True)
     
     def __str__(self):
         return self.user.username
