27acb19ce0f8af13a37b9483f2d48adfbfa06844
ci-sumi <tharayilsurendransumi@gmail.com>
2025-01-04 15:12:40 +0000

    Craete  UserProfileForm

---
+++ profiles/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+from .models import UserProfile
+
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        exclude = ('user',)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'default_phone_number': 'Phone Number',
+            'default_county': 'County, State or Province',
+            'default_postcode': 'Postcode',
+            'default_town_or_city': 'Town or City',
+            'default_street_address1': 'Street Address 1',
+            'default_street_address2': 'Street Address 2',
+            'default_country': 'Country',
+        }
+        self.fields['default_phone_number'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if field != 'default_country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                    self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'form-control border-black rounded-0'
+            self.fields[field].label = False
+                
+                
+        
