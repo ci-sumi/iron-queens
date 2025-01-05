fe99f64b49f39d8fc082485d04830141dc83a307
ci-sumi <tharayilsurendransumi@gmail.com>
2025-01-05 19:16:14 +0000

    Add url pattern for add_product

--- products/urls.py
+++ products/urls.py
@@ -19,9 +19,9 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('',views.all_products, name='products'),
-    path('<int:id>/',views.product_detail, name='product_detail'),
-    
+        path('',views.all_products, name='products'),
+        path('<int:id>/',views.product_detail, name='product_detail'),
+        path('add/',views.add_product, name='add_product'),
     
 ]
 
