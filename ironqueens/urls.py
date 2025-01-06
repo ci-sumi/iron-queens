8eb147bc30684c63a39d7e1b6f7446f554befad0
ci-sumi <tharayilsurendransumi@gmail.com>
2025-01-06 18:07:15 +0000

    Add URL routing for services app in the main URL configuration

--- ironqueens/urls.py
+++ ironqueens/urls.py
@@ -27,5 +27,6 @@ urlpatterns = [
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
+    path('services/', include('services.urls')),
 ] + static(settings.MEDIA_URL,document_root= settings.MEDIA_ROOT)+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
