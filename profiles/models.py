aec65f15e5f262438be88e847b09472bb0e8fb1e
ci-sumi <tharayilsurendransumi@gmail.com>
2025-01-06 12:25:37 +0000

    Add Contact model and alter Userprofile

--- profiles/models.py
+++ profiles/models.py
@@ -27,5 +27,21 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     # Existing users: just save the profile
     instance.userprofile.save()
     
+class Contact(models.Model):
+    ENQUIRY_TYPE = (
+        ('product', 'Product Enquiry'),
+        ('sales', 'Sales Enquiry'),
+        ('service', 'Service Enquiry'),
+        ('other', 'Other')
+    )
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=254)
+    enquiry_type = models.CharField(max_length=20, choices=ENQUIRY_TYPE)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.name}-{self.get_enquiry_type_display()}" 
+    
            
     
