13a2efcb4be283adf078aa6247482184fbefb29b
ci-sumi <tharayilsurendransumi@gmail.com>
2025-01-06 12:29:41 +0000

    Add URL pattern for Contact view

--- profiles/urls.py
+++ profiles/urls.py
@@ -21,6 +21,7 @@ from .import views
 urlpatterns = [
     path('',views.profile, name='profile'),
     path('order_history/<order_number>',views.order_history, name='order_history'),
+    path('contact/',views.contact, name='contact'),
     
 ]
 
