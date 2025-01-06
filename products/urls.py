8ae143657fb3dabed4d4b9c4862dcdd00b3a7e9e
ci-sumi <tharayilsurendransumi@gmail.com>
2025-01-06 07:41:24 +0000

    Add URL for deleting a product by product ID

--- products/urls.py
+++ products/urls.py
@@ -22,7 +22,8 @@ urlpatterns = [
         path('',views.all_products, name='products'),
         path('<int:id>/',views.product_detail, name='product_detail'),
         path('add/',views.add_product, name='add_product'),
-        path('edit/<int:id>/',views.edit_product, name='edit_product'),
+        path('edit/<int:product_id>/',views.edit_product, name='edit_product'),
+        path('delete/<int:product_id>/',views.delete_product, name='delete_product'),
     
 ]
 
