e8c100af997f2692220e2d38a044281ef0b0f22d
ci-sumi <tharayilsurendransumi@gmail.com>
2025-01-07 14:43:45 +0000

    Create TestimonialForm excluding user field for logged-in user association

--- services/forms.py
+++ services/forms.py
@@ -1,7 +1,14 @@
 from django import forms
-from .models import Service
+from .models import Service,Testimonial
 
 class ServiceForm(forms.ModelForm):
     class Meta:
         model = Service
         fields =['name', 'description', 'image']
+        
+class TestimonialForm(forms.ModelForm):
+    class Meta:
+        model = Testimonial
+        fields =['service', 'content']
+    
+    
