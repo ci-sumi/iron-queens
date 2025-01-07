b52b1676015fe8001859d25b31465ed93af46b4d
ci-sumi <tharayilsurendransumi@gmail.com>
2025-01-07 14:42:36 +0000

    Add Testimonial model with user, service, and content fields

--- services/models.py
+++ services/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
+
 
 
 # Create your models here.
@@ -10,3 +12,15 @@ class Service(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Testimonial(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    service = models.ForeignKey(Service, on_delete=models.CASCADE,related_name="testimonials")
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return  f"{self.user.username} - {self.service.name}"
+    
+    
