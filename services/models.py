5c407770eae0cd635b0fc7730c01ea2e5c02267c
ci-sumi <tharayilsurendransumi@gmail.com>
2025-01-14 17:46:35 +0000

    Update Service model to use AWS S3 for image storage

--- services/models.py
+++ services/models.py
@@ -8,7 +8,8 @@ from django.contrib.auth.models import User
 class Service(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    image = CloudinaryField('image', null=True, blank=True)
+    image = models.ImageField(upload_to='services/',null=True, blank=True)
+    # image = CloudinaryField('image', null=True, blank=True)
 
     def __str__(self):
         return self.name
