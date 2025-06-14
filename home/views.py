4b03e538fc4ffe1d6b695f130cacc45728d1341d
ci-sumi <tharayilsurendransumi@gmail.com>
2025-06-14 20:28:59 +0100

    Modify custom 404 view

--- home/views.py
+++ home/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 def my_view(request):
     return render(request,'home/index.html')
 
-def custom_404(request, exception=None):
+def custom_404(request, exception):
     return render(request, '404.html',status=404)
