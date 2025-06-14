2d76dd87bf5f7bd037432be9063d569883a80beb
ci-sumi <tharayilsurendransumi@gmail.com>
2025-06-14 17:14:07 +0100

    Configure handler404 in project

--- home/views.py
+++ home/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def my_view(request):
     return render(request,'home/index.html')
+
+def custom_404(request, exception=None):
+    return render(request, '404.html',status=404)
--- ironqueens/urls.py
+++ ironqueens/urls.py
@@ -18,6 +18,10 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import handler404
+from home.views import custom_404  # Import your fixed view
+
+handler404 = custom_404  # Connect Django to your view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
