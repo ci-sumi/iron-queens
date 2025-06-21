acbbf6844db20354ff72b511adcc0b9a6e60e779
ci-sumi <tharayilsurendransumi@gmail.com>
2025-06-21 23:12:41 +0100

    Clean he code after testing

--- ironqueens/settings.py
+++ ironqueens/settings.py
@@ -12,10 +12,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-# import cloudinary
-# import cloudinary.uploader
-# import cloudinary.api  
-# import cloudinary_storage
 from dotenv import load_dotenv
 from urllib.parse import urlparse
 
@@ -28,30 +24,19 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-
-
-# SECURITY WARNING: don't run with debug turned on in production!
-
-
-
 # Configure Cloudinary credentials
 # CLOUDINARY_STORAGE = {
 #     'CLOUD_NAME': os.environ.get('CLOUD_NAME'),
 #     'API_KEY': os.environ.get('API_KEY'),
 #     'API_SECRET': os.environ.get('API_SECRET')
 # }
-ALLOWED_HOSTS = ['iron-queens.herokuapp.com','iron-queens-9a116e72c4d2.herokuapp.com', '127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS = ['iron-queens.herokuapp.com',
+                 'iron-queens-9a116e72c4d2.herokuapp.com',
+                 '127.0.0.1',
+                 '0.0.0.0', ]
 
 SITE_ID = 1
 
-# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-# print(CLOUDINARY_STORAGE)
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
@@ -68,10 +53,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'cloudinary',
-    # 'cloudinary_storage',
-    
-    # Thrird-party apps
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
@@ -80,20 +61,13 @@ INSTALLED_APPS = [
     'bag',
     'checkout',
     'crispy_forms',
-    'crispy_bootstrap5', 
+    'crispy_bootstrap5',
     'django_countries',
     'profiles',
     'services',
     'widget_tweaks',
     'storages',
-    
-
-    
-    # 'allauth.socialaccount.providers.google', #Add Google provider
 ]
-
-
-
 
 
 MIDDLEWARE = [
@@ -115,18 +89,16 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'templates'),
-                 os.path.join(BASE_DIR, 'templates','allauth'),
+                 os.path.join(BASE_DIR, 'templates', 'allauth'),
                  ],
-                  
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request',# required by all auth
+                'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'bag.contexts.bag_contents', 
-                
+                'bag.contexts.bag_contents',
             ],
         },
     },
@@ -138,7 +110,7 @@ TEMPLATES = [
 # Account settings
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
-ACCOUNT_EMAIL_VERIFICATION = 'mandatory'  # Options: "mandatory", "optional", "none"  # Options: "username", "email", "username_email"
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 # LOGIN_REDIRECT_URL = '/accounts/login/'
@@ -147,16 +119,6 @@ LOGOUT_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'ironqueens.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 tmpPostgres = urlparse(os.getenv("DATABASE_URL"))
 
@@ -176,16 +138,28 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'UserAttributeSimilarityValidator'
+        ),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'MinimumLengthValidator'
+        ),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'CommonPasswordValidator'
+        ),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'NumericPasswordValidator'
+        ),
     },
 ]
 
@@ -212,13 +186,9 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Static files settings
-
-# STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-# # Set Cloudinary as the default file storage
 DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR,"static"),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # MEDIA_URL = '/media/'
@@ -226,13 +196,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # AWS S3 settings
 if 'USE_AWS' in os.environ:
-    #catche settings
     AWS_S3_OBJECT_PARAMETERS = {
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
         'CacheControl': 'max-age=94608000',
     }
-    
-    
     AWS_STORAGE_BUCKET_NAME = 'iron-queens'
     AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
@@ -250,12 +217,10 @@ if 'USE_AWS' in os.environ:
 
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
-
-#stripe
 STRIPE_CURRENCY = 'usd'
-STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY','')
-STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY','')
-STRIPE_WH_SECRET_KEY = os.getenv('STRIPE_WH_SECRET_KEY','')
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET_KEY = os.getenv('STRIPE_WH_SECRET_KEY', '')
 
 
 if 'DEVELOPMENT' in os.environ:
